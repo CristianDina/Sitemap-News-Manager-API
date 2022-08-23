553427321c3a60cb4f221f4915b3c7bbe3a173f8
stefania.iuga <iugastefania26@gmail.com>
2022-08-23 16:19:15 +0300

    init

---
+++ src/main/java/com/pillarglobal/sitemap/news/manager/api/SitemapNewsManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pillarglobal.sitemap.news.manager.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitemapNewsManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitemapNewsManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pillarglobal/sitemap/news/manager/api/SitemapNewsManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pillarglobal.sitemap.news.manager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitemapNewsManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
