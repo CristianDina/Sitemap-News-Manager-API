17a8426d096ee8917be8d99e5c8c896243adb4f8
cristian.dina <cristi.dina93@yahoo.com>
2022-09-14 18:40:54 +0300

    terraform re-init

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "sitemap-news-manager-db" {
   db_name              = "news"
   engine_version       = "8.0.28"
   instance_class       = "db.t3.micro"
-  identifier           = "sitemap-news-manager-db"
+  identifier           = "sitemap-news-manager-db-2"
   username             = "admin"
   password             = "admin123"
   vpc_security_group_ids = ["sg-03b208ce83581f3d5"]
